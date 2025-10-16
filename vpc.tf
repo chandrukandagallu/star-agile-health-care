69cf819d7d94fd72f2db7812ce7d898a2ff28e33
chandrukandagallu <chandrukandagallu@gmail.com>
2025-10-16 09:11:14 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,21 @@
+# terraform_files/vpc.tf
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "3.14.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "medicure-vpc"
+  cidr = "10.0.0.0/16"
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  map_public_ip_on_launch = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+  tags = {
+    Name = "medicure-vpc"
   }
 }
